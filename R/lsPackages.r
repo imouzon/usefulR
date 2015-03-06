e524560b3b5bde73d6ef3c3dcea7d07cdf25f0db
imouzon <imouzon@iastate.edu>
2015-03-06 11:59:11 -0600

    unexpected )

--- R/lsPackages.r
+++ R/lsPackages.r
@@ -2,7 +2,7 @@
 #'
 #' Purpose: lists the packages loaded since the start of the R session
 #' Creation Date: 06-03-2015
-#' Last Modified: Fri Mar  6 11:55:47 2015
+#' Last Modified: Fri Mar  6 11:58:53 2015
 #'
 #' @param ret logical value, optional argument
 #' @param pr logical value, optional argument
@@ -13,7 +13,7 @@
 lsPackages = function(ret=FALSE,pr=TRUE,lib=FALSE){
    added.pkgs = (.packages())[which(!(.packages()) %in% getOption("defaultPackages"))]
    if(lib){
-      cat.packs = paste0('library(', added.pkgs, ')', collapse='\n'))
+      cat.packs = paste0('library(', added.pkgs, ')', collapse='\n')
    }else{
       cat.packs = paste0(added.pkgs, collapse='\n')
    }
