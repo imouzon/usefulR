01479ae1be3943b5cae2f773fbd1fab1178f6528
imouzon <imouzon@iastate.edu>
2015-03-06 15:39:33 -0600

    fixing lsPackages

--- R/lsPackages.r
+++ R/lsPackages.r
@@ -2,7 +2,7 @@
 #'
 #' Purpose: lists the packages loaded since the start of the R session
 #' Creation Date: 06-03-2015
-#' Last Modified: Fri Mar  6 15:05:17 2015
+#' Last Modified: Fri Mar  6 15:37:22 2015
 #'
 #' @param ret logical value, optional argument
 #' @param pr logical value, optional argument
@@ -10,8 +10,9 @@
 #' @export
 #' @examples
 #' lsPackages(FALSE,TRUE,TRUE)
-lsPackages = function(ret=FALSE,pr=TRUE,lib=FALSE){
-   added.pkgs = (.packages())[which(!(.packages()) %in% getOption("defaultPackages"))]
+lsPackages = function(ret=FALSE,pr=TRUE,lib=FALSE,ignore.packages='base'){
+   added.pkgs = (.packages())[which(!(.packages()) %in% c(getOption("defaultPackages"),ignore.packages))]
+
    if(lib){
       cat.packs = paste0('library(', added.pkgs, ')', collapse='\n')
    }else{
@@ -19,4 +20,12 @@ lsPackages = function(ret=FALSE,pr=TRUE,lib=FALSE){
    }
    if(pr) cat(cat.packs)
    if(ret) return(added.pkgs)
+
+   #sapply(1:length(added.pkgs), function(i) getDepList(added.pkgs[i]))
+   #getDepList()
+   #search()
+   #(.packages())[which(!(.packages()) %in% c(getOption("defaultPackages"),'base'))]
+   #available.packages()
+   #tools:::package.dependencies(added.pkgs)
+   #tools:::pkgDepends(c('ggplot2'))
 }
