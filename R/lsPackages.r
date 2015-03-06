7feff0ac0005b7ab62c86601645ff722b88b09ce
imouzon <imouzon@iastate.edu>
2015-03-06 16:07:35 -0600

    lspackages updated

--- R/lsPackages.r
+++ R/lsPackages.r
@@ -2,7 +2,7 @@
 #'
 #' Purpose: lists the packages loaded since the start of the R session
 #' Creation Date: 06-03-2015
-#' Last Modified: Fri Mar  6 16:03:18 2015
+#' Last Modified: Fri Mar  6 16:06:36 2015
 #'
 #' @param ret logical value, optional argument
 #' @param pr logical value, optional argument
@@ -18,7 +18,7 @@ lsPackages = function(ret=FALSE,pr=TRUE,lib=FALSE,ignore.packages='base',kntr=FA
    }else{
       cat.packs = paste0(added.pkgs, collapse='\n')
    }
-   if(lib && pr) cat.packs = paste('\\\\end{verbatim}\n', paste0('\\\\hlwkd{library}\\\\hlstd{(',added.pkgs,')}'), '\\\\begin{verbatim}',collapse='\n')
+   if(lib && pr) cat.packs = paste('\\\\end{verbatim}', paste0('\\\\hlwkd{library}\\\\hlstd{(',added.pkgs,')}',collapse='\n'), '\\\\begin{verbatim}',collapse='\n')
    if(pr) cat(cat.packs)
    if(ret) return(added.pkgs)
 }
