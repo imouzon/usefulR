e4c459ca0b0f80f751efad0da4b2d54fb93f4192
imouzon <imouzon@iastate.edu>
2015-05-04 16:52:56 -0500

    Created pandoc-like template for homework

--- R/knitrSetup.r
+++ R/knitrSetup.r
@@ -1,61 +1,78 @@
 #' Set knitr up the way I like it
 #'
 #' This code sets some of the values for knitr that I like.
-#' @param rootDir character value, optional argument
-#' @param use.tikz logical value, optional argument
+#' @param rootDir character value, optional argument (defaults to working directory)
+#' @param use.tikz logical value, optional argument (default FALSE)
+#' @param cache.use.time logical value, optional argument (default FALSE)
+#' @param fig.folder character value, optional argument (default 'graphics')
+#' @param fig.prefix character value, optional argument (default 'fig_')
 #' @export
 #' @examples
 #' knitrSetup(use.tikz = FALSE)
-knitrSetup = function(rootDir = FALSE, use.tikz = FALSE){
+knitrSetup = function(rootDir=getwd(), use.tikz=FALSE, cache.use.time=FALSE, fig.folder='graphics', fig.prefix='fig_', cache.folder='cache', cache.prefix='out_', formatR.arrow=TRUE, code.width=50, fig.height=5, fig.width=5, fig.align='center', fig.show='hold', crop=TRUE, tidy=TRUE, fig.pos='H', par=TRUE, tikzLatex='/usr/texbin/pdflatex', tikz.sanitize=TRUE, tikz.crop=TRUE, return_opts=NULL){
    require('knitr')
-   if(rootDir == FALSE) rootDir <- getwd()
-
-   #use date in figure names or cache files?
-   cache.use.time = FALSE
 
    #store figures in the fig folder, prefix with 'graphics_'
-   fig.folder = paste0(rootDir,'/graphics/')
-   fig.prefix = 'graphics'
-   fig.path = paste0(fig.folder,fig.prefix,'_')
+   fig.folder = file.path(rootDir,fig.folder)
+   fig.path = paste0(fig.folder,'/',fig.prefix)
 
    #cached output stored in cache.path
-   cache.folder = paste0(rootDir,'/cache/')
-   cache.prefix = 'out'
-   cache.path = paste0(cache.folder,cache.prefix,'_')
+   cache.folder = file.path(rootDir,'cache')
+   cache.path = paste0(cache.folder,'/',cache.prefix)
 
-   options(formatR.arrow=TRUE,width=50)
+   #These are arguments for formatR which tidys up the code
+   Roptions = list("formatR.arrow"=formatR.arrow,"width"=code.width)
 
-   #figure options
-   fig.w = 5
-   fig.h = 5
-   opts_chunk$set(
-      fig.path=fig.path,
-      cache.path=cache.path,
-      fig.align='center', 
-      fig.width=fig.w, 
-      fig.height=fig.h, 
-      fig.show='hold', 
-      crop=TRUE,
-      tidy=TRUE,
-      fig.pos = 'H',
-      par=TRUE
+   #knit options
+   knitrSetup_knit_opts = list(NULL)
+
+   #chunk options
+   knitrSetup_chunk_opts = list(
+      "fig.path"=fig.path,
+      "cache.path"=cache.path,
+      "fig.align"=fig.align, 
+      "fig.width"=fig.width, 
+      "fig.height"=fig.height, 
+      "fig.show"=fig.show, 
+      "crop"=crop,
+      "par"=par,
+      "fig.pos"=fig.pos,
+      "tidy"=tidy
    )
 
+   #fix the color errors
+   knitrSetup_hooks_opts = list("document" = function(x){sub('\\usepackage[]{color}', '\\usepackage[]{xcolor}', x, fixed = TRUE)})
+
+   #if we are supposed to use tikz then what?
    if(use.tikz != FALSE){
       require(tikzDevice)
-      options(tikzLatex = '/opt/local/bin/pdflatex')
-      opts_chunk$set(dev='tikz', sanitize=TRUE,crop=TRUE)
-      knit_hooks$set(par=function(before, options, envir){if(before && options$fig.show!='none') par(mar=c(4,4,.1,.1), cex.lab=.95, cex.axis=.9, mgp=c(2,.7,0), tcl=-.3)}, crop=hook_pdfcrop)
-      knit_hooks$set(true.font=function(before,options,envir) if(before && !is.null(options$out.width)) dev.args = list(pointsize=10*as.numeric(strsplit(options$out.width,'\\\\')[[1]][1])))
+      Roptions = usefulR::merge_lists(list(Roptions,list("tikzLatex" = tikzLatex)),FALSE)
+      knitrSetup_chunk_opts = usefulR::merge_lists(list(knitrSetup_chunk_opts,list("dev"='tikz',"sanitize"=TRUE,"crop"=TRUE)),FALSE)
+      knitrSetup_hooks_opts = usefulR::merge_lists(list(knitrSetup_hooks_opts,
+         list("par"=function(before, options, envir){
+            if(before && options$fig.show!='none') par(mar=c(4,4,.1,.1), cex.lab=.95, cex.axis=.9, mgp=c(2,.7,0), tcl=-.3)
+         },
+         "crop"=hook_pdfcrop
+         )),
+         FALSE
+      )
+      #knit_hooks$set(true.font=function(before,options,envir){
+      #   if(before && !is.null(options$out.width)) 
+      #      dev.args = list(pointsize=10*as.numeric(strsplit(options$out.width,'\\\\')[[1]][1]))
+      #})
    }
 
-   #fix the color errors
-   knit_hooks$set(document = function(x) {
-              sub('\\usepackage[]{color}', 
-                  '\\usepackage[]{xcolor}', x, fixed = TRUE)
-   })
-
-   fig.msg = paste('By defualt figures will be stored in ',fig.path,'and have dimension',fig.w,'by',fig.h)
+   fig.msg = paste('By defualt figures will be stored in',fig.path,'and have dimension',fig.width,'by',fig.height)
    cache.msg = paste('By default cache will be stored in',cache.path)
-   message('knitr has the following options:\n',fig.msg,'\n',cache.msg,sep='   ')
+   message(paste('knitr has the following options:',fig.msg,cache.msg,sep='\n'))
+
+   if(is.null(return_opts)){
+      opts_knit$set(knitrSetup_knit_opts)
+      opts_chunk$set(knitrSetup_chunk_opts)
+      knitr_hooks$set(knitrSetup_hooks_opts)
+      options(Roptions)
+   }else{
+      return_opts = list("knitr" = list("opts_knit"=knitrSetup_knit_opts, "opts_chunk" = knitrSetup_chunk_opts, "knit_hooks" = knitrSetup_hooks_opts),"R" = Roptions)
+      return(return_opts)
+   }
 }
--- R/merge_lists.r
+++ R/merge_lists.r
@@ -2,7 +2,7 @@
 #'
 #' Purpose: merge two or more lists together
 #' Creation Date: 19-03-2015
-#' Last Modified: Fri Mar 20 00:02:17 2015
+#' Last Modified: Mon May  4 16:49:18 2015
 #'
 #' @param lists a list of list, optional argument
 #' @param append.lists logical argument, optional argument
@@ -12,12 +12,28 @@
 #' list1 = list('dogs'=c('chows','beagles'), cows='milk', food = 'hot dogs')
 #' list2 = list('dogs'='doberman', cows=c('milk','beef'), friends = 'nick')
 #' list3 = list('dogs'='puppy', cows=c('barnyard','beef'), girls='peach', friends = 'ian')
-#' merge_lists(list(list1,list2,list3))
+#' usefulR::merge_lists(list(list1,list2,list3))
 #' 
 
 merge_lists = function(lists,append.lists=TRUE,descending.priority=FALSE){
+   #remove lists that are null
+   lists = list(format$knitr$opts_knit, knitrSetup.opts$knitr$opts_knit)
+   lists.in = lists
+   for(i in length(lists):1){
+      if(is.null(names(lists[[i]]))){
+         lists[[i]] = NULL
+      }else{
+         dropmem = which(names(lists[[i]]) == "")
+         if(length(dropmem) > 0){
+            lists[[i]][dropmem] = NULL
+         }
+      }
+   }
+   if(length(lists) == 0){
+      return(lists.in[[1]])
+   }
+   sapply(1:length(lists), function(i) is.null(lists[[i]]))
    if(descending.priority) lists = lists[length(lists):1]
-   l1 = lists[[1]]
    for(i in 2:length(lists)){
       l2 = lists[[i]]
       common.names = names(l1)[which(names(l1) %in% names(l2))]
